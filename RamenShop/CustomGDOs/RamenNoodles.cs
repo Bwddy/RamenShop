a8d66a7bd37dca4af91296df2c302c7894ddca94
Bwddy <buddygus2015@gmail.com>
2023-08-30 22:22:29 -0400

    Add project files.

---
+++ RamenShop/CustomGDOs/RamenNoodles.cs
@@ -0,0 +1,20 @@
+﻿using KitchenData;
+using KitchenLib.Customs;
+using KitchenLib.Utils;
+using UnityEngine;
+using TestCubes;
+
+namespace RamenShop
+{
+    namespace Customs
+    {
+        public class RamenNoodles : CustomItem
+        {
+            public override string UniqueNameID => "RamenNoodles";
+
+            public override GameObject Prefab => TestCubeManager.GetPrefab("RamenNoodles", 0.2f, 0.2f, 0.2f, MaterialUtils.GetExistingMaterial("Wood - Default"), false);
+
+            public override Appliance DedicatedProvider => (Appliance)GDOUtils.GetCustomGameDataObject<RamenProvider>().GameDataObject;
+        }
+    }
+}
---
+++ RamenShop/CustomGDOs/RamenProvider.cs
@@ -0,0 +1,27 @@
+﻿using KitchenData;
+using KitchenLib.Customs;
+using KitchenLib.Utils;
+using System.Collections.Generic;
+using TestCubes;
+using UnityEngine;
+
+namespace RamenShop
+{
+    namespace Customs
+    {
+        public class RamenProvider : CustomAppliance
+        {
+            public override string UniqueNameID => "RamenProvider";
+
+            public override GameObject Prefab => TestCubeManager.GetPrefab("RamenProvider", 1, 1, 1, MaterialUtils.GetExistingMaterial("Wood - Default"));
+
+            public override List<(Locale, ApplianceInfo)> InfoList => new List<(Locale, ApplianceInfo)> { ((Locale)1, new ApplianceInfo
+            {
+            Name = "RamenBox",
+            Description = "Provides Ramen Noodles",
+            }) };
+
+            public override List<IApplianceProperty> Properties => new List<IApplianceProperty> { (IApplianceProperty)(object)KitchenPropertiesUtils.GetUnlimitedCItemProvider(GDOUtils.GetCustomGameDataObject<RamenNoodles>().GameDataObject.ID) };
+        }
+    }
+}
